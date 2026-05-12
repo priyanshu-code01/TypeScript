97cb966c4271f8d9d133a34b32d59a46fbb1cb40
Priyanshu Singh <priyanshu.code01@gmail.com>
2026-05-12 23:16:07 +0530

    bigint

---
+++ 02 Data Types/Bigint.js
@@ -0,0 +1,5 @@
+// let bignumber = 934984389484843978493; // wrong
+let bignumber = 934984389484843978493n; // correct
+let x = 4n;
+console.log(bignumber + x);
+export {};
---
+++ 02 Data Types/Bigint.ts
@@ -0,0 +1,8 @@
+// let bignumber = 934984389484843978493; // wrong
+let bignumber: bigint = 934984389484843978493n; // correct
+
+let x: bigint = 4n;
+
+console.log(bignumber + x)
+
+export {}
