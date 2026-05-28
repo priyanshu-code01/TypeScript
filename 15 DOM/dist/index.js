a4aaabef2f1a263105cd76ef1a09349839b850c5
Priyanshu Singh <priyanshu.code01@gmail.com>
2026-05-28 06:03:00 +0530

    DOM

---
+++ 15 DOM/dist/index.js
@@ -0,0 +1,5 @@
+let headingEl = document.querySelector('h1');
+console.log(headingEl === null || headingEl === void 0 ? void 0 : headingEl.textContent);
+let anchorT = document.querySelector('a');
+console.log(anchorT === null || anchorT === void 0 ? void 0 : anchorT.href);
+//# sourceMappingURL=index.js.map
---
+++ 15 DOM/src/index.ts
@@ -0,0 +1,5 @@
+let headingEl = document.querySelector('h1');
+console.log(headingEl?.textContent)
+
+let anchorT = document.querySelector('a');
+console.log(anchorT?.href)
